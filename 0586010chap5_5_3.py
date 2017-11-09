349b2ed020a6312b088d4d89b8da18523d368329
Katie <klks0304@users.noreply.github.com>
2017-11-09 00:25:28 -0600

    Add files via upload

---
+++ 0586010chap5_5_3.py
@@ -0,0 +1,27 @@
+#5.3
+
+import matplotlib.pyplot as plt
+
+CPU_burst = [6,6,4,6,4,13,13,13]
+guess_time = [10,8,6,6,5,9,11,12]
+
+#output figure of 5.3
+#from scipy.interpolate import spline
+x = range(len(guess_time))
+y = guess_time
+plt.figure(figsize=(6,8))
+plt.xlim(1,max(x)+1)
+plt.ylim(0,max(CPU_burst)+1)
+plt.plot(x,guess_time)
+plt.plot(x,CPU_burst)
+plt.legend(['guess (Taui)','CPU burst (ti)'], loc='upper left')
+plt.title('Prediction of the Length of the Next CPU Burst')
+plt.xlabel('time ->')
+plt.axes().set_aspect(0.4)
+plt.suptitle('wolfe', style='italic', y=-0.3, fontsize=12)
+plt.grid(True)
+#plt.xticks(0,8,1)
+#cell_text = ['CPU burst (ti)']
+Table = plt.table(cellText=[CPU_burst+["..."],guess_time+["..."]],rowLabels=['CPU burst (ti)','"guess"(Taui)'],loc="bottom", bbox=[0.3, -0.5, 0.7, 0.3])
+
+plt.show()
---
+++ 0586010chap5_5_4.py
@@ -0,0 +1,36 @@
+#5.4
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+#variable setting
+CPU_burst = [6,6,4,6,4,13,13,13,100,50] #the last 2 are randomly added
+guess_time = [10]
+#tau = 10
+#t = 6
+alpha = 0.5
+for i in range(len(CPU_burst)-1):
+    guess_time.append(alpha*guess_time[i]+(1-alpha)*CPU_burst[i])
+
+#output figure of 5.4
+#from scipy.interpolate import spline
+x = range(len(guess_time))
+y = guess_time
+plt.figure(figsize=(6,6))
+plt.xlim(1,max(x)+1)
+plt.ylim(0,max(CPU_burst)+1)
+plt.plot(x,guess_time)
+plt.plot(x,CPU_burst)
+plt.xticks(np.arange(0,len(CPU_burst),1))
+plt.legend(['guess (Taui)','CPU burst (ti)'], loc='upper left')
+plt.title('Prediction of the Length of the Next CPU Burst')
+plt.xlabel('time ->')
+plt.grid(True)
+#plt.axes().set_aspect(0.1)
+plt.suptitle('wolfe', style='italic', y=-0.5, fontsize=12)
+#cell_text = ['CPU burst (ti)']
+Table = plt.table(cellText=[CPU_burst+["..."],guess_time+["..."]],rowLabels=['CPU burst (ti)','"guess"(Taui)'],loc="bottom", bbox=[0, -0.2, 1.2 ,0.1])
+#the table may not be on a perfect location
+#can be adjust after output
+
+plt.show()
